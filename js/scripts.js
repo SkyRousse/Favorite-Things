adc87a505c0a5894fbc88aa53ef032a642bfbcd2
Rony Nasr and Sky Rousse <rony@>
2016-06-06 09:37:34 -0700

    add script to grab values from inputs and put them into arrays and then output those arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-/*This file is for your custom js.  All yours*/
+
+
+$(function() {
+  $("form#fav-things").submit(function(event) {
+    event.preventDefault();
+  var  favorites = [$("input#fav1").val(),$("input#fav2").val(), $("input#fav3").val(), $("input#fav4").val()];
+  var newFavorites = [favorites[1],favorites[0],favorites[2]];
+  newFavorites.push(favorites[3]);
+  console.log(favorites);
+  console.log(newFavorites);
+  $("ul.display").append("<li>"+newFavorites[0]+"</li>");
+  $("ul.display").append("<li>"+newFavorites[1]+"</li>");
+  $("ul.display").append("<li>"+newFavorites[2]+"</li>");
+  $("ul.display").append("<li>"+newFavorites[3]+"</li>");
+  });
+});
